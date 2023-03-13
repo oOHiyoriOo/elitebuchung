8b688209b5b1440003ac52d415cb3ff926117c9a
Nisi@win10 <wolf.sf75@googlemail.com>
2023-03-13 20:15:21 +0100

    done?

--- assets/js/booking.js
+++ assets/js/booking.js
@@ -6,17 +6,75 @@ setInterval(() => { // update every second because lol.
 
 
 const days = ["mo","di","mi","do","fr","sa","do"]
+var books = {}
+
 
 function get_books(){
   $.get("/book", function(response) {
-    let bookings = JSON.parse(response)
-    let booking_table = $('#bookings-table')
-    for(i in bookings){
-      
-      let booking = bookings[i]
-      let day_index = days.indexOf( String(booking['day']).toLowerCase() ) + 2
-      let hour_index = Number.parseInt( booking['hour'] )
+      books = JSON.parse(response)
+      apply_books(books, $('#room_select').val());
+  }); 
+}
+
+
+function apply_books(bookings, raum){
+
+  for (let hour = 0; hour < $('#bookings-table > tbody > tr').length ; hour++) { // go trough the the hours. 
+    for (let day = 2; day < days.length; day++) { // go trough the the hours. 
+      let cell = $(`#bookings-table > tbody > tr:nth-child(${hour}) > td:nth-child(${day})`);
+      if (cell.html() !== "-") { cell.html("-"); }
+    }
+  }
+
+  for(let i in bookings){
+    let booking = bookings[i]
+    
+    let day_index = days.indexOf( String(booking['day']).toLowerCase() ) + 2
+    let hour_index = Number.parseInt( booking['hour'] )
+    if( (booking.room).toLowerCase() == raum.toLowerCase() ){
       $(`#bookings-table > tbody > tr:nth-child(${hour_index}) > td:nth-child(${day_index})`).html(`${booking['teacher']}<br>(${booking['fach']})`)
     }
-  });
+  
+  }
+}
+
+
+
+
+$('td:contains("-")').click(function() {
+  if( this.textContent != "-") return false;
+
+  var stunde = $(this).closest('tr').find('td:first-child').text();
+  var tag = $(this).closest('table').find('thead th').eq($(this).index()).text();
+
+  if(! ["sa","so"].includes( tag.toLowerCase().replace(/ /g,"").replace(/\n/g,"") )){
+    $('#modal-hour').val(stunde.replace(/ /g,""));
+    $('#modal-day').val(tag.replace(/ /g,""));
+    $('#booking_window').show();
+  }
+});
+
+function saveBooking(){
+  let data = {
+    "day":$('#modal-day').val(),
+    "hour":$('#modal-hour').val(),
+    "teacher":$('#modal-teach').val(),
+    "fach": $('#modal-name').val(),
+    "room": $('#room_select').val()
+  }
+
+  $('#modal-teach').val("")
+  $('#modal-name').val("")
+  
+  if( ["day","hour","teacher","fach","room"].every(key => data[key] !== '') ){
+
+    $.ajax({
+      type: "POST",
+      url: "/book",
+      data: data,
+      success: function(response) {
+        console.log( response.statusCode )
+      }
+    });
+  }
 }
--- server.js
+++ server.js
@@ -10,49 +10,84 @@ const cfg = require('./config.json');
 
 // Create the Webserver
 const app = express();
-	// extras i need.
-	app.use(express.static('assets'))
-	// parse application/x-www-form-urlencoded
-	app.use(bodyParser.urlencoded({ extended: false }))
-	// parse application/json
-	app.use(bodyParser.json())
-	// Cookie handling
+// extras i need.
+app.use(express.static('assets'))
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+// Cookie handling
 
-	// header manipulation
-	app.use(function (req, res, next) {
-		res.setHeader("X-Powered-By","Variant ist doof.")
-		next();
+// header manipulation
+app.use(function (req, res, next) {
+	res.setHeader("X-Powered-By", "Elite")
+	next();
+});
+
+
+// Set the views directory and view engine to EJS
+app.set('views', './html');
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => { res.redirect('/index.html') })
+
+
+
+app.get('/index.html', (req, res) => {
+	let bookings = fs.readFileSync(process.cwd() + "/data/bookings.json", { encoding: 'utf-8' });
+
+	res.render('index.ejs', { bookings });
+});
+
+
+// Catch .php requests and render .ejs files with the same filename if they exist
+app.get('*.html', (req, res, next) => {
+	const ejsPath = path.join(__dirname, 'html', `${(req.path).slice(0, -4)}ejs`);
+
+	console.log(ejsPath)
+
+	// Check if the corresponding .ejs file exists
+	fs.access(ejsPath, fs.constants.F_OK, (err) => {
+		if (err) {
+			// If the .ejs file doesn't exist, move on to the next middleware
+			return next();
+		}
+
+		// If the .ejs file exists, render it
+		res.render(ejsPath);
 	});
-
-
-// Routes
-app.get('/', (req, res) => {
-	res.send( fs.readFileSync(process.cwd()+"/html/index.html",{encoding:'utf-8'}) );
-});
-
-app.post('/book', (req,res) => {
-    let data = req.body;
-    if( ["day","hour","teacher","fach"].every(key => data[key] !== '') ){
-
-        let db = JSON.parse( fs.readFileSync(process.cwd()+"/data/bookings.json",{encoding:'utf-8'}) );
-
-        db.push(data)
-
-        fs.writeFileSync(process.cwd()+"/data/bookings.json",JSON.stringify(db),{encoding:'utf-8'})
-        res.status(200).send({'msg':'data saved!'})
-    }else{
-        res.status(500).send({"msg":"missing or invalid data."})
-    }
-});
-
-app.get('/book', (req,res) => {
-    res.send( fs.readFileSync(process.cwd()+"/data/bookings.json",{encoding:'utf-8'}) )
 });
 
 
 
-// Hallo Motherfucker
 
-app.listen(cfg.port,cfg.host, () => {
-    console.log(`App listening at http://${cfg.host}:${cfg.port}`);
+app.post('/book', (req, res) => {
+	let data = req.body;
+	if (["day", "hour", "teacher", "fach","room"].every(key => data[key] !== '')) {
+
+		let db = JSON.parse(fs.readFileSync(process.cwd() + "/data/bookings.json", { encoding: 'utf-8' }));
+
+		db.push(data)
+
+		fs.writeFileSync(process.cwd() + "/data/bookings.json", JSON.stringify(db), { encoding: 'utf-8' })
+		res.status(200).send({ 'msg': 'data saved!' })
+	} else {
+		res.status(500).send({ "msg": "missing or invalid data." })
+	}
+});
+
+app.get('/book', (req, res) => {
+	res.send(fs.readFileSync(process.cwd() + "/data/bookings.json", { encoding: 'utf-8' }))
+});
+
+// reset the bookings by rewriting the file. 
+app.post('/82851faa-e000-4d43-9ac7-44b17a1d15b2', (req, res) => {
+	console.log("Database Reset Started!")
+	fs.writeFileSync(process.cwd() + "/data/bookings.json", JSON.stringify([]), { encoding: 'utf-8' })
+	res.send({'msg':'okay'})
+})
+
+app.listen(cfg.port, cfg.host, () => {
+	console.log(`App listening at http://${cfg.host}:${cfg.port}`);
 });
